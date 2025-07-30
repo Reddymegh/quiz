9097c35f29c78c1a977cd5cec0ea1eea961d2dcd
Reddy_meghana <160709275+Reddymegh@users.noreply.github.com>
2025-07-30 10:26:26 +0530

    Create Question.java

---
+++ Question.java
@@ -0,0 +1,25 @@
+package question;
+
+public class Question {
+    private String questionText;
+    private String[] options;
+    private int correctAnswerIndex;
+
+    public Question(String questionText, String[] options, int correctAnswerIndex) {
+        this.questionText = questionText;
+        this.options = options;
+        this.correctAnswerIndex = correctAnswerIndex;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public String[] getOptions() {
+        return options;
+    }
+
+    public boolean isCorrect(int choice) {
+        return choice == correctAnswerIndex;
+    }
+}
